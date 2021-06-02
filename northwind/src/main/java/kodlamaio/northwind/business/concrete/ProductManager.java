9f795158f57a7039daa2747b44c643cba72a471c
TunaFigankaplan <swaxztn@gmail.com>
2021-06-02 15:07:38 +0300

    02.06.2021

---
+++ northwind/src/main/java/kodlamaio/northwind/business/concrete/ProductManager.java
@@ -0,0 +1,37 @@
+package kodlamaio.northwind.business.concrete;
+
+import java.util.List;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
+import kodlamaio.northwind.entities.concrete.Product;
+
+@Service
+public class ProductManager implements ProductService{
+
+	private ProductDao productDao;
+	
+	@Autowired
+	public ProductManager(ProductDao productDao) {
+		super();
+		this.productDao = productDao;
+	}
+
+	@Override
+	public DataResult<List<Product>> getAll() {
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(),"Data Listed");
+	}
+
+	@Override
+	public Result add(Product product) {
+		this.productDao.save(product);
+		return new SuccessResult("Ürün Eklendi");
+	}
+}
